04b57dae184411884987560df72273770d6c44d5
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-09-28 05:32:50 -0300

    Form component for Update User Profile infos

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,17 +2,22 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/layout";
 import { HomePage } from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={
-                <Layout>
+                <Layout showHero={true}>
                     <HomePage />
                 </Layout>
             }/>
             <Route path="/auth-callback" element={<AuthCallbackPage />}/>
-            <Route path="/user-profile" element={<span>User Profile page</span>}/>
+            <Route path="/user-profile" element={
+                <Layout>
+                    <UserProfilePage />
+                </Layout>
+            }/>
             <Route path="*" element={<Navigate to="/" />}/>
         </Routes>
     )
