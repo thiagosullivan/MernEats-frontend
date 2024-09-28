520dc15c84a3013ce3c0dc8ad2669803f38950d2
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-09-28 05:32:00 -0300

    Adding User Profile page

---
+++ src/pages/UserProfilePage.tsx
@@ -0,0 +1,10 @@
+import UserProfileForm from "@/forms/user-profile-form/UserProfile";
+
+
+const UserProfilePage = () => {
+    return (
+        <UserProfileForm />
+    )
+}
+
+export default UserProfilePage;
